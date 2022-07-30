7a7e37286e7e14e7efb57c3f4f1c69379b76e7ab
GnaixEuy <sgghcj@live.com>
2022-07-30 16:41:45 +0800

    :beers: 提交了算法入门计划初级第十天内容

---
+++ src/main/java/算法入门/第十天/反转链表.java
@@ -0,0 +1,59 @@
+package 算法入门.第十天;
+
+import java.util.Stack;
+
+/**
+ * <img src="http://blog.gnaixeuy.cn/wp-content/uploads/2022/06/bug.png"/>
+ *
+ * <p>项目： algorithm-java </p>
+ *
+ * @author GnaixEuy
+ * @date 2022/7/30
+ * @see <a href="https://github.com/GnaixEuy"> GnaixEuy的GitHub </a>
+ */
+public class 反转链表 {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    class Solution {
+        public ListNode reverseList(ListNode head) {
+            Stack<ListNode> listNodes = new Stack<>();
+            while (head != null) {
+                listNodes.push(head);
+                head = head.next;
+            }
+            ListNode result = new ListNode();
+            head = result;
+            while (!listNodes.isEmpty()) {
+                result.next = listNodes.pop();
+                result = result.next;
+            }
+            result.next = null;
+            return head.next;
+        }
+    }
+}
---
+++ src/main/java/算法入门/第十天/合并两个有序链表.java
@@ -0,0 +1,56 @@
+package 算法入门.第十天;
+
+/**
+ * <img src="http://blog.gnaixeuy.cn/wp-content/uploads/2022/06/bug.png"/>
+ *
+ * <p>项目： algorithm-java </p>
+ *
+ * @author GnaixEuy
+ * @date 2022/7/30
+ * @see <a href="https://github.com/GnaixEuy"> GnaixEuy的GitHub </a>
+ */
+public class 合并两个有序链表 {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    class Solution {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            if (list1 == null) {
+                return list2;
+            } else if (list2 == null) {
+                return list1;
+            } else if (list1.val < list2.val) {
+                list1.next = mergeTwoLists(list1.next, list2);
+                return list1;
+            } else {
+                list2.next = mergeTwoLists(list2.next, list1);
+                return list2;
+            }
+        }
+
+    }
+}
